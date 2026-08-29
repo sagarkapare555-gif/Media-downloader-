9429627e4875a2cdd950a49425f04b16250e418d
sagarkapare555-gif <sagarkapare555@gmail.com>
2026-08-29 23:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,5 +85,6 @@ app.post('/download', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Downloader running on port 3000!'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Downloader running on port ' + PORT));
 
